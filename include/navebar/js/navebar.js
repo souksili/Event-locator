5ea24c8416824c5bf2a241e7be28272e65fcbea0
Reddy Massidi semi <massidisemi@yahoo.fr>
2024-10-10 00:11:52 +0200

    init branch

--- include/navebar/js/navebar.js
+++
@@ -1,9 +0,0 @@
-// Load the navbar HTML and inject it into the page
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('include/navebar/navebar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar-placeholder').innerHTML = data;
-        })
-        .catch(error => console.error('Error loading the navbar:', error));
-});
